2c7a9c6ae56af52b41dc961fd345757a54fdf85e
joephy527 <joephy527@gmail.com>
2024-01-22 15:51:08 -0800

    add custom animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,25 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      animation: {
+        "spin-slow": "spin 200s reverse linear infinite",
+        "spin-mid": "spinadjust 200s linear infinite",
+      },
+      keyframes: {
+        spinadjust: {
+          "0%": { transform: "rotate(30deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
