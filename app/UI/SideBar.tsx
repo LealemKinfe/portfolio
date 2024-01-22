54bd2f9cb29dd32e38be32af8ced0eedc1a499fd
joephy527 <joephy527@gmail.com>
2024-01-22 15:53:09 -0800

    add some custom styles to the Navbar and make it responsive

--- app/UI/SideBar.tsx
+++ app/UI/SideBar.tsx
@@ -1,27 +1,85 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const SideBar = () => {
   return (
-    <section className="w-1/5 h-screen border bg-[#2F2F2F]">
-      SIDEBAR
-      <div className="p-5 text-white -z-10">
-        <div className="relative rounded-full bg-[#707070]/50 p-5">
-          <Link href="/" className="absolute -right-1 top-1 text-2xl rotate-45">
-            Home
-          </Link>
-          <Link href="/" className="absolute -left-5 top-1 text-2xl -rotate-45">
-            LinkTree
-          </Link>
-          <Link
-            href="/"
-            className="absolute -bottom-6 left-1/2 -translate-x-1/2 text-2xl rotate-180"
-          >
-            About
-          </Link>
-          <div className="rounded-full p-24 bg-[#C3C3C3]/50"></div>
+    <>
+      {/* Desktop design */}
+      <section className="hidden lg:block px-1 w-[280px] h-screen bg-[#2F2F2F]">
+        <div className="p-5 text-white my-5">
+          <div className="relative rounded-full bg-[#707070]/50 p-5 text-2xl animate-spin-slow">
+            <Link href="/" className="absolute -right-1 top-2.5 rotate-45">
+              <span className="-rotate-12 inline-block">H</span>
+              <span className="inline-block -translate-y-0.5">o</span>
+              <span className="inline-block -translate-y-0.5 rotate-3">m</span>
+              <span className="inline-block rotate-[9deg]">e</span>
+            </Link>
+            <Link href="/" className="absolute -left-5 top-2.5 -rotate-45">
+              <span className="inline-block translate-y-1 -rotate-[18deg]">
+                L
+              </span>
+              <span className="inline-block translate-y-[1px] -rotate-12">
+                i
+              </span>
+              <span className="inline-block -translate-y-[1px] -rotate-[9deg]">
+                n
+              </span>
+              <span className="inline-block -translate-y-0.5 -rotate-3">k</span>
+              <span className="inline-block -translate-y-0.5 rotate-3">T</span>
+              <span className="inline-block -translate-y-[1px] rotate-[9deg]">
+                r
+              </span>
+              <span className="inline-block translate-y-[1px] rotate-[15deg]">
+                e
+              </span>
+              <span className="inline-block translate-y-[5px] rotate-[18deg]">
+                e
+              </span>
+            </Link>
+            <Link
+              href="/about"
+              className="absolute -bottom-7 left-1/2 -translate-x-1/2 rotate-180"
+            >
+              <span className="inline-block translate-y-1 -rotate-12">A</span>
+              <span className="inline-block translate-y-[1px] -rotate-6">
+                b
+              </span>
+              <span className="inline-block translate-y-[1px]">o</span>
+              <span className="inline-block translate-y-0.5 rotate-[9deg]">
+                u
+              </span>
+              <span className="inline-block translate-y-1 rotate-[15deg]">
+                t
+              </span>
+            </Link>
+            <div className="rounded-full p-24 bg-[#C3C3C3]/50"></div>
+          </div>
         </div>
-      </div>
-    </section>
+
+        {/* <div className="h-full">
+        <Image
+          src="/assets/Path13.svg"
+          width={50}
+          height={80}
+          alt="decorative vector"
+          className="mt-32"
+        />
+      </div> */}
+      </section>
+
+      {/* Mobile design */}
+      <section className="lg:hidden bg-[#2F2F2F] sm:p-10 p-5 text-white flex justify-center gap-5 sm:text-2xl">
+        <Link href="/" className="z-10">
+          Home
+        </Link>
+        <Link href="/" className="z-10">
+          LinkTree
+        </Link>
+        <Link href="/about" className="z-10">
+          About
+        </Link>
+      </section>
+    </>
   );
 };
 
