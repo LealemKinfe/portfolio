f56621199fea23a536e64a1fce8e17b2ab4a416d
joephy527 <joephy527@gmail.com>
2024-01-24 14:59:43 -0800

    add project page data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,117 +1,132 @@
-export const cardData = [
-  [
-    {
-      imgSrc: "/assets/uiux/Cards.png",
-      title: "Project Esss",
-      link: "/",
-    },
-    {
-      imgSrc: "/assets/uiux/Group249.png",
-      title: "Project Ablaze",
-      link: "/",
-    },
-    {
-      imgSrc: "/assets/uiux/Group250.png",
-      title: "Project KTS",
-      link: "/",
-    },
-    {
-      imgSrc: "/assets/uiux/Group251.png",
-      title: "Project ESKC",
-      link: "/",
-    },
-    {
-      imgSrc: "/assets/uiux/Group253.png",
-      title: "Project Madeya",
-      link: "/",
-    },
-  ],
-  [
-    {
-      imgSrc: "/assets/uiux/Cards.png",
-      title: "Project Esss",
-      link: "/",
-    },
-    {
-      imgSrc: "/assets/uiux/Group249.png",
-      title: "Project Ablaze",
-      link: "/",
-    },
-    {
-      imgSrc: "/assets/uiux/Group250.png",
-      title: "Project KTS",
-      link: "/",
-    },
-    {
-      imgSrc: "/assets/uiux/Group251.png",
-      title: "Project ESKC",
-      link: "/",
-    },
-    {
-      imgSrc: "/assets/uiux/Group253.png",
-      title: "Project Madeya",
-      link: "/",
-    },
-  ],
-  [
-    {
-      imgSrc: "/assets/uiux/Cards.png",
-      title: "Project Esss",
-      link: "/",
-    },
-    {
-      imgSrc: "/assets/uiux/Group249.png",
-      title: "Project Ablaze",
-      link: "/",
-    },
-    {
-      imgSrc: "/assets/uiux/Group250.png",
-      title: "Project KTS",
-      link: "/",
-    },
-    {
-      imgSrc: "/assets/uiux/Group251.png",
-      title: "Project ESKC",
-      link: "/",
-    },
-    {
-      imgSrc: "/assets/uiux/Group253.png",
-      title: "Project Madeya",
-      link: "/",
-    },
-  ],
-  [
-    {
-      imgSrc: "/assets/uiux/Cards.png",
-      title: "Project Esss",
-      link: "/",
-    },
-    {
-      imgSrc: "/assets/uiux/Group249.png",
-      title: "Project Ablaze",
-      link: "/",
-    },
-    {
-      imgSrc: "/assets/uiux/Group250.png",
-      title: "Project KTS",
-      link: "/",
-    },
-    {
-      imgSrc: "/assets/uiux/Group251.png",
-      title: "Project ESKC",
-      link: "/",
-    },
-    {
-      imgSrc: "/assets/uiux/Group253.png",
-      title: "Project Madeya",
-      link: "/",
-    },
-  ],
-];
+export const imageData: any = {
+  uiux: "/assets/icons/uiux.svg",
+  graphics: "/assets/icons/graphics.svg",
+  branding: "/assets/icons/branding.svg",
+  logo: "/assets/icons/logo.svg",
+};
 
-export const imageData = [
-  "/assets/icons/uiux.svg",
-  "/assets/icons/graphics.svg",
-  "/assets/icons/branding.svg",
-  "/assets/icons/logo.svg",
-];
+export const cardData: any = {
+  uiux: [
+    {
+      imgSrc: "/assets/uiux/Cards.png",
+      title: "Project Esss",
+      link: "projectesss",
+    },
+    {
+      imgSrc: "/assets/uiux/Group249.png",
+      title: "Project Ablaze",
+      link: "projectablaze",
+    },
+    {
+      imgSrc: "/assets/uiux/Group250.png",
+      title: "Project KTS",
+      link: "projectkts",
+    },
+    {
+      imgSrc: "/assets/uiux/Group251.png",
+      title: "Project ESKC",
+      link: "projecteskc",
+    },
+    {
+      imgSrc: "/assets/uiux/Group253.png",
+      title: "Project Madeya",
+      link: "projectmadeya",
+    },
+  ],
+  graphics: [
+    {
+      imgSrc: "/assets/uiux/Cards.png",
+      title: "Project Esss",
+      link: "/",
+    },
+    {
+      imgSrc: "/assets/uiux/Group249.png",
+      title: "Project Ablaze",
+      link: "/",
+    },
+    {
+      imgSrc: "/assets/uiux/Group250.png",
+      title: "Project KTS",
+      link: "/",
+    },
+    {
+      imgSrc: "/assets/uiux/Group251.png",
+      title: "Project ESKC",
+      link: "/",
+    },
+    {
+      imgSrc: "/assets/uiux/Group253.png",
+      title: "Project Madeya",
+      link: "/",
+    },
+  ],
+  branding: [
+    {
+      imgSrc: "/assets/uiux/Cards.png",
+      title: "Project Esss",
+      link: "/",
+    },
+    {
+      imgSrc: "/assets/uiux/Group249.png",
+      title: "Project Ablaze",
+      link: "/",
+    },
+    {
+      imgSrc: "/assets/uiux/Group250.png",
+      title: "Project KTS",
+      link: "/",
+    },
+    {
+      imgSrc: "/assets/uiux/Group251.png",
+      title: "Project ESKC",
+      link: "/",
+    },
+    {
+      imgSrc: "/assets/uiux/Group253.png",
+      title: "Project Madeya",
+      link: "/",
+    },
+  ],
+  logo: [
+    {
+      imgSrc: "/assets/uiux/Cards.png",
+      title: "Project Esss",
+      link: "/",
+    },
+    {
+      imgSrc: "/assets/uiux/Group249.png",
+      title: "Project Ablaze",
+      link: "/",
+    },
+    {
+      imgSrc: "/assets/uiux/Group250.png",
+      title: "Project KTS",
+      link: "/",
+    },
+    {
+      imgSrc: "/assets/uiux/Group251.png",
+      title: "Project ESKC",
+      link: "/",
+    },
+    {
+      imgSrc: "/assets/uiux/Group253.png",
+      title: "Project Madeya",
+      link: "/",
+    },
+  ],
+};
+
+export const projectData: any = {
+  projectesss: {
+    name: "Esss Learning",
+    client: "Ethiopian Space Science Society",
+    about:
+      "is a new initiative by the Ethiopian Space Science Society (ESSS), dedicated to making space science and technology education accessible to everyone. At ESSS, we're passionate about exploring the mysteries of the cosmos and sharing that knowledge with the world.",
+    imgUrl: "/assets/Group258.svg",
+    caseStudy:
+      "is a new initiative by the Ethiopian Space Science Society (ESSS), dedicated to making space science and technology education accessible to everyone. At ESSS, we're passionate about exploring the mysteries of the cosmos and sharing that knowledge with the world.",
+    demo: "is a new initiative by the Ethiopian Space Science Society (ESSS), dedicated to making space science and technology education accessible to everyone. At ESSS, we're passionate about exploring the mysteries of the cosmos and sharing that knowledge with the world.",
+    youtubeLink: "/",
+    demoLink: "/",
+  },
+};
