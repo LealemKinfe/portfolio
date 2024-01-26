4139cdfbbe6a6fff8475d6c50f3688e49119aa3f
joephy527 <joephy527@gmail.com>
2024-01-26 04:25:27 -0800

    add project page data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -124,10 +124,67 @@ export const projectData: any = {
       "is a new initiative by the Ethiopian Space Science Society (ESSS), dedicated to making space science and technology education accessible to everyone. At ESSS, we're passionate about exploring the mysteries of the cosmos and sharing that knowledge with the world.",
     imgUrl: "/assets/uiux/Learning.png",
     caseStudy:
-      "is a new initiative by the Ethiopian Space Science Society (ESSS), dedicated to making space science and technology education accessible to everyone. At ESSS, we're passionate about exploring the mysteries of the cosmos and sharing that knowledge with the world.",
-    caseStudy2:
-      "is a new initiative by the Ethiopian Space Science Society (ESSS), dedicated to making space science and technology education accessible to everyone. At ESSS, we're passionate about exploring the mysteries of the cosmos and sharing that knowledge with the world.",
+      "In the vast realm of space science education, a need emerged for a beacon of knowledge, a platform seamlessly connecting enthusiasts with the wonders of the cosmos. This case study delves into the transformative journey of designing and developing Learn.ethiosss.org, an online learning platform by the Ethiopian Space Science Society (ESSS).",
     youtubeLink: "/",
-    demoLink: "/",
+    demoLink:
+      "https://xd.adobe.com/view/e765054f-f3b6-4a56-97df-61eb53b15f76-b236/",
+    caseStudyLink:
+      "https://docs.google.com/presentation/d/1xjxFhgydyaFRSenDK5pmaPz4ryQ2XcvNzvfHVHEg2kE/edit?usp=sharing",
+  },
+  projectablaze: {
+    name: "PayEt",
+    client: "Ablaze Labs",
+    about:
+      "PayET is a digital payment wallet application developed by Ablaze Labs. It serves as a secure and user-friendly platform for users to engage in various financial transactions.",
+    imgUrl: "/assets/uiux/Learning.png",
+    caseStudy:
+      "In the dynamic landscape of digital payments, Ablaze Labs introduces PayET, a cutting-edge payment wallet app designed to redefine user experiences in the realm of financial transactions. This UI/UX test endeavors to encapsulate the essence of PayET, a secure and user-friendly application that seamlessly integrates into users' daily lives, offering a plethora of functionalities for effortless financial management.",
+    youtubeLink: "/",
+    demoLink:
+      "https://xd.adobe.com/view/2bab40fc-3c28-474d-af64-9c24d5c97999-7ef8/",
+    caseStudyLink:
+      "https://docs.google.com/presentation/d/1N4Ct2Bje5STRRywAb4Vo0FT0BY9FPnNITF3okDbZpMU/edit?usp=sharing",
+  },
+  projectkts: {
+    name: "KTS",
+    client: "KTS",
+    about:
+      "Kelal brings you a digital ticketing platform accessible through your smartphone and/or a dedicated RFID card.",
+    imgUrl: "/assets/uiux/Learning.png",
+    caseStudy:
+      "In the heart of Ethiopia's evolving transportation landscape, the KTS (Knowledgeable Transportation Solutions) system stands as a beacon of modernity and innovation, fundamentally reshaping the way Ethiopians traverse their cities. This case study delves into the user interface (UI) and user experience (UX) design journey that propelled KTS to the forefront of the country's transportation revolution.",
+    youtubeLink: "/",
+    demoLink:
+      "https://xd.adobe.com/view/19434c94-013c-4a40-b306-d485657038b4-4c10/",
+    caseStudyLink:
+      "https://docs.google.com/presentation/d/12rJCcUYUh96LSwUqE-jSNhDu4XPt8wULdsXlSyYZfCA/edit?usp=sharing",
+  },
+  projecteskc: {
+    name: "ESKC",
+    client: "SSGI",
+    about:
+      "Embarking on a cosmic journey of education and inspiration, the Ethiopian Space Kids Club (ESKC) has been a beacon for young minds aspiring to explore the wonders of space science and geospatial technology. This UI/UX case study delves into the transformative project aimed at refining the digital experience for ESKC members, making their educational odyssey even more engaging, interactive, and accessible.",
+    imgUrl: "/assets/uiux/Learning.png",
+    caseStudy:
+      "The Ethiopian Space Kids Club, operating under the Space Science & Geospatial Institute (SSGI), seeks to instill a passion for space sciences in children and talented young individuals. Despite the club's commitment to hands-on learning experiences, there was a need to bridge the gap between physical activities and the online realm. This case study aims to address the challenges faced by ESKC in providing an enriched, seamless, and user-friendly digital platform for its diverse range of educational programs and club activities.",
+    youtubeLink: "/",
+    demoLink:
+      "https://drive.google.com/file/d/1NN1MraqcG85D7JUYkxZiYG-0utdZTa8j/view?usp=sharing",
+    caseStudyLink:
+      "https://docs.google.com/presentation/d/1KdOO5ANGxotmvTMIXAXKlQoIISJSlXhjbSrLzCAW5vY/edit?usp=sharing",
+  },
+  projectmadeya: {
+    name: "Madia Go",
+    client: "Madia Go",
+    about:
+      "Mobile application that aims to revolutionize the roadside assistance industry by providing drivers platform for immediate help during emergencies, including fuel delivery.",
+    imgUrl: "/assets/uiux/Learning.png",
+    caseStudy:
+      "In the fast-paced world of transportation, unforeseen challenges on the road can quickly turn into emergencies. Imagine a mobile application that not only anticipates these predicaments but also redefines the entire roadside assistance industry. This case study dives into the development and impact of a groundbreaking mobile application designed to be a lifeline for drivers in distress, introducing a seamless platform for immediate help during emergencies, including the innovative provision of on-demand fuel delivery.",
+    youtubeLink: "/",
+    demoLink:
+      "https://drive.google.com/file/d/1hbIVLbE_MYaG_xCbbxp-PHBX94iubtNn/view?usp=sharing",
+    caseStudyLink:
+      "https://docs.google.com/presentation/d/1z5u7E-NXynnLMFNYpKtAf63WMbDU7O2wpkZjOVITH-o/edit?usp=sharing",
   },
 };
